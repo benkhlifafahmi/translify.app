ca08c8c1ea55905efacf9cb2f56a82117ad595da
Fahmi Ben Khalifa <benkhlifa.fahmi@icloud.com>
2026-05-17 18:39:29 +0200

    pixel

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata, Viewport } from "next";
 import { Fraunces, Hanken_Grotesk } from "next/font/google";
-import Script from "next/script";
 import "./globals.css";
 import { Providers } from "@/components/providers";
 import { StructuredData } from "@/components/structured-data";
@@ -126,6 +125,15 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     >
       <head>
         <StructuredData />
+        {/* Facebook Pixel */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init','1289655489982440');fbq('track','PageView');`,
+          }}
+        />
+        <noscript>
+          <img height="1" width="1" style={{ display: "none" }} src="https://www.facebook.com/tr?id=1289655489982440&ev=PageView&noscript=1" alt="" />
+        </noscript>
         {/* Plausible — cookieless, privacy-friendly analytics. Loads only
             when NEXT_PUBLIC_PLAUSIBLE_DOMAIN is set (production), so
             local development doesn't pollute production stats. */}
@@ -139,15 +147,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       </head>
       <body>
         <Providers>{children}</Providers>
-        <Script id="facebook-pixel" strategy="afterInteractive">{`
-          !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');
-          fbq('init', '1289655489982440');
-          fbq('track', 'PageView');
-        `}</Script>
-        <noscript>
-          {/* eslint-disable-next-line @next/next/no-img-element */}
-          <img height="1" width="1" style={{ display: "none" }} src="https://www.facebook.com/tr?id=1289655489982440&ev=PageView&noscript=1" alt="" />
-        </noscript>
       </body>
     </html>
   );
